a1f2982fe9a33827f4d2ea7a70f5a3a81b4942e8
Patrick McGreevy <patrick.mcgreevy@wsu.edu>
2020-01-22 11:54:09 -0800

    Same as last commit but for real this time.

--- create_graphics/generate_grapics.py
+++ create_graphics/generate_grapics.py
@@ -1,8 +1,10 @@
+import sys
 import matplotlib.pyplot as plt
 import matplotlib.cm as mplcm
 import matplotlib.colors as colors
 import numpy as np
 import pandas as pd
+
 
 def save_heatmap(data, rowlabels, collabels, title, imgname):
     plt.matshow(data)
@@ -11,27 +13,34 @@ def save_heatmap(data, rowlabels, collabels, title, imgname):
     cax = ax.matshow(data)
 
     fig.colorbar(cax)
-    ax.set_xticks(np.arrange(data.shape[0]))
-    ax.set_yticks(np.arrange(data.shape[1]))
+    #ax.set_xticks(np.arange(data.shape[0]))
+    #ax.set_yticks(np.arange(data.shape[1]))
     ax.tick_params(top=False, bottom=True, labeltop=False, labelbottom=True)
 
-    ax.set_xticklabels(rowlabels)
-    ax.set_yticklabels(collabels)
+    plt.xlabel(rowlabels)
+    plt.ylabel(collabels)
 
     for edge, spine in ax.spines.items():
-        spine.set_visible=False
+        spine.set_visible = False
 
-    ax.grid(which='minor', colow='w', linestile='-', linewidth=3)
+    ax.grid(which='minor', color='w', linestyle='-', linewidth=3)
     ax.set_title(title, pad=15)
 
     plt.tight_layout()
     plt.savefig(imgname)
 
 
+if (len(sys.argv) < 6):
+    print('Error: Too few arguments')
+    print('Please include \'src\' \'dest\' \'X-Axis Label\' \'Y-Axis Label\' \'Graph Title\'', 'in that order.')
+    print('Labels and titles with spaces in them must be surounded by \'NAME\' single quotes.')
+else:
+    src = sys.argv[1]
+    dst = sys.argv[2]
+    x_label = sys.argv[3]
+    y_label = sys.argv[4]
+    title = sys.argv[5]
 
+    data = pd.read_csv(src)
 
-
-data = pd.read_csv('../../processed_data/exp2_data.csv')
-
-
-save_heatmap(data, data.shape[0], data.shape[1], 'Activation Ring Over Time', 'img1.png')
+    save_heatmap(data, x_label, y_label, title, dst)
--- v1/transform_data.py
+++ v1/transform_data.py
@@ -53,39 +53,45 @@ def test_collisions(circles_list):
 
 #path = input('Please enter the path to your data folder: ')
 #path = '../../raw_data/d'
-path = '../../raw_data/121319 Force measurements morning.lif_Series012_Crop001'
-print(path)
+if len(sys.argv) < 5:
+    print('Missing arguments.\nPlease include \'srs\' \'dest\' \'x_ref\' \'y_ref\'\nIn that order.')
+else:
+    #path = '../../raw_data/121319 Force measurements morning.lif_Series012_Crop001'
+    path = sys.argv[1]
+    print('Source: ' + path)
+    dest = sys.argv[2]
+    print('Destination: ' + dest)
+    #x_y = input('Please enter the (X, Y) coordinates of your reference pixel')
+    #x, y = tuple(x_y.split(','))
+    #ref_x, ref_y = (217, 264)
+    ref_x, ref_y = (int(sys.argv[3]), int(sys.argv[4]))
+    print('(x_ref, y_ref): (', ref_x, ',', ref_y, ')\n', sep=' ')
 
-#x_y = input('Please enter the (X, Y) coordinates of your reference pixel')
-#x, y = tuple(x_y.split(','))
-#ref_x, ref_y = (217, 264)
-ref_x, ref_y = (241, 228)
-#print(x +', ' + y)
+    print('Loading all movie data...')
+    files = glob.glob(os.path.join(path, "*.txt"))
+    files.sort(key=numeric_sort_key)
 
-print('Loading all movie data...')
-files = glob.glob(os.path.join(path, "*.txt"))
-files.sort(key=numeric_sort_key)
+    data = [pd.read_csv(path, sep=' ', header=None).drop(columns=[512]) for path in files]
+    print('Done')
+    # Step 1 get list of circle coordinates for each distance from reference pixel
+    x_max, y_max = (max([i.shape[0] for i in data]), max([i.shape[1] for i in data]))
+    r_max = min([ref_x, x_max - ref_x, ref_y, y_max - ref_y])
 
-data = [pd.read_csv(path, sep=' ', header=None).drop(columns=[512]) for path in files]
-print('Done')
-# Step 1 get list of circle coordinates for each distance from reference pixel
-x_max, y_max = (max([i.shape[0] for i in data]), max([i.shape[1] for i in data]))
-r_max = min([ref_x, x_max - ref_x, ref_y, y_max - ref_y])
-
-print('Calculating circle indices...')
-circles = make_circle_lists(ref_x, ref_y, r_max)
-print('Done')
-# Step 2 create new DF where each col is a distance and row is a timestamp. Entries are circle average for dist and time
-print('Averaging circle values...')
-dist_averages = pd.DataFrame([[0 for i in range(r_max)] for j in range(len(data))])
-for t in range(len(data)):
-    # Time t; average the cells of each circle and insert them into t,radius
-    for r in range(r_max):
-        #dist_averages = dist_averages.append(pd.DataFrame(np.mean([data[t][x][y] for (x,y) in circles[r]])), ignore_index=True)
-        dist_averages[r][t] = np.mean([data[t][x][y] for (x,y) in circles[r]])
+    print('Calculating circle indices...')
+    circles = make_circle_lists(ref_x, ref_y, r_max)
+    print('Done')
+    # Step 2 create new DF where each col is a distance and row is a timestamp. Entries are circle average for dist and time
+    print('Averaging circle values...')
+    dist_averages = pd.DataFrame([[0 for i in range(r_max)] for j in range(len(data))])
+    for t in range(len(data)):
+        # Time t; average the cells of each circle and insert them into t,radius
+        for r in range(r_max):
+            #dist_averages = dist_averages.append(pd.DataFrame(np.mean([data[t][x][y] for (x,y) in circles[r]])), ignore_index=True)
+            dist_averages[r][t] = np.mean([data[t][x][y] for (x,y) in circles[r]])
 
 
-print('Done.')
+    print('Done.')
 
 
-dist_averages.to_csv('../processed_data')
+    #dist_averages.to_csv('../processed_data')
+    dist_averages.to_csv(dest)
